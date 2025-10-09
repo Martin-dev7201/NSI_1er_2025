ad4644cb988bb547dc21e9bae7412bb798cd5d3e
Martin-dev7201 <martinjavourezb2b@gmail.com>
2025-10-09 16:57:22 +0200

    Update Exo5.py

--- Language Python/Exo5.py
+++ Language Python/Exo5.py
@@ -1,1 +1,36 @@
-sdbvgfn ,
+def leet_speak(texte):
+    # On crée un dictionnaire avec les remplacements à faire
+    remplacements = {
+        'A': '4',
+        'B': '8',
+        'X': '8',
+        'E': '3',
+        'I': '1',
+        'L': '1',
+        'K': 'X',
+        'S': '5',
+        'T': '7',
+        'O': '0'
+    }
+
+    # On met tout le texte en majuscules pour simplifier
+    texte = texte.upper()
+
+    # On crée une nouvelle chaîne vide pour stocker le résultat
+    resultat = ""
+
+    # On parcourt chaque lettre du texte
+    for lettre in texte:
+        if lettre in remplacements:
+            # Si la lettre est dans le dictionnaire, on la remplace
+            resultat += remplacements[lettre]
+        else:
+            # Sinon, on garde la lettre telle quelle
+            resultat += lettre
+
+    # On renvoie le texte transformé
+    return resultat
+
+
+# Exemple d'utilisation :
+print(leet_speak("LEET SPEAK"))

23e5ce969f4db2abb4ad66c1cb6e45eaef636d08
Martin-dev7201 <martinjavourezb2b@gmail.com>
2025-10-09 16:04:58 +0200

    Update Exos4.py

--- Language Python/Exos4.py
+++ Language Python/Exos4.py
@@ -3,16 +3,16 @@ import math   # pour utiliser la racine carrée
 def equation_2nd_degre(a, b, c):
     """Résout une équation du second degré : a*x² + b*x + c = 0"""
     delta = b**2 - 4*a*c
-    print("Le discriminant Δ =", delta)
+    print(f"Le discriminant Δ ={delta}")
 
     if delta < 0:
         print("Il n’y a pas de solution réelle.")
     elif delta == 0:
         x = -b / (2*a)
-        print("Une seule solution :", x)
+        print(f"Une seule solution :{n}")
     else:
         x1 = (-b - math.sqrt(delta)) / (2*a)
         x2 = (-b + math.sqrt(delta)) / (2*a)
         print("Deux solutions :")
-        print("x1 =", x1)
-        print("x2 =", x2)
+        print(f"x1 ={x1}")
+        print(f"x2 ={x2}")

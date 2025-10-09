5ac1996bcd0f2531363b1360477bff438292e488
Martin-dev7201 <martinjavourezb2b@gmail.com>
2025-10-09 10:30:02 +0200

    Create Exo2.py

---
+++ Language Python/Exo2.py
@@ -0,0 +1,6 @@
+def diviseurs(n):
+    """Affiche les diviseurs du nombre n"""
+    print("Les diviseurs de", n, "sont :")
+    for i in range(1, n + 1):   # On teste tous les nombres de 1 à n
+        if n % i == 0:          # Si la division tombe juste (pas de reste)
+            print(i)

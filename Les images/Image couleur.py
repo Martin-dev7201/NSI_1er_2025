46d7fa15901b668b33183002de0a7530243f6831
Martin <martinjavourezb2b@gmail.com>
2026-03-26 10:34:17 +0100

    Update Image couleur.py

--- Les images/Image couleur.py
+++ Les images/Image couleur.py
@@ -36,21 +36,15 @@ def symetrie_verticale():
     afficherImage(img)
 
 def appliquer_masque():
-    hauteur = len(img)
-    largeur = len(img[0])
-    img_resultat = Image.new("RGB", (largeur, hauteur))
-    for y in range(hauteur):
-        for x in range(largeur):
-            pixel_base = img[y][x]
-            pixel_m = img_2[y][x]
-            val_m = pixel_m[0] if isinstance(pixel_m, (list, tuple)) else pixel_m
-            nouveaux_canaux = []
-            for c in pixel_base:
-                if c < 128:
-                    res = (2 * c * val_m) // 255
-                else:
-                    res = 255 - (2 * (255 - c) * (255 - val_m)) // 255
-                nouveaux_canaux.append(max(0, min(255, int(res))))
-            img_resultat.putpixel((x, y), tuple(nouveaux_canaux))        
-    afficherImage(img_resultat)
+   hauteur = len(img)
+    largeur=len(img[0])
+    liste=len(img[0][0])
+    for ligne in range(hauteur) :
+        for colonne in range (largeur):
+            for color in range(liste):
+                if masque[ligne][colonne]==255:
+                    img[ligne][colonne][color]=img[ligne][colonne][color]-30
+                if img[ligne][colonne][color]<0:
+                    img[ligne][colonne][color]=0
+    afficherImage(img)
     
